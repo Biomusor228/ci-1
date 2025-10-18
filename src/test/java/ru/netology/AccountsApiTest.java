ae5c4a1c62d3419d271aa744ecd9fd46c96556d2
Biomusor228 <fiodoroff.mir@yandex.ru>
2025-10-18 23:17:48 +0300

    add tests and CI config

---
+++ src/test/java/ru/netology/AccountsApiTest.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class AccountsApiTest {
+    @Test
+    void shouldValidateAccountsSchema() {
+        given()
+                .baseUri("http://localhost:9999")
+                .when()
+                .get("/api/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
